98743d6d753cf531bdffc16fb030f26eaed61fcf
Stephen Selke <stephen.selke@gmail.com>
2015-10-06 13:33:13 -0500

    First Commit

---
+++ backend/objects/Player.java
@@ -0,0 +1,23 @@
+// Programmers:  Stephen Selke, Bradley Golden, Chris Griffith
+// Assignment:  Project 2, Monopoly
+// Date:        October 6, 2015
+// Description: 
+//              
+
+public class Player 
+{
+	private int money;              // amount of money a player has
+	private BoardLocation location; // track the location of a player on the board
+	private String token;           // piece that represents player
+	private Property[] properties;  // list of properties owned by the player
+
+	public Player() 
+	//POST:  a new player will be created with $0, starting at Go, no properties
+	{
+		money = 0;
+		//location = BoardLocation.GO;
+		
+	}
+
+	public 
+}
