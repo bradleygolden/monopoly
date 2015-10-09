3ae1be458d649568c7d9207dcd4270967d8a233d
Bradley Golden <bradleygolden@10-4-232-135.vlan890.client.wireless.uic.edu>
2015-10-09 12:46:45 -0500

    created property class

---
+++ backend/objects/Property.java
@@ -0,0 +1,21 @@
+// Programmers:  Stephen Selke, Bradley Golden, Chris Griffith
+// Assignment:  Project 2, Monopoly
+// Date:        October 6, 2015
+// Description:
+//
+
+public abstract class Property 
+{
+    // Player owner; // The owner of the property
+    private int price; // The purchase price of the property
+    private int baseRent; // The cost of rent on a given space without improvements 
+
+    public Property()
+    {
+    }
+
+    public int getRent()
+    {
+    }
+
+}
