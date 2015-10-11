cd584fb7808b38638e2aeb7272dc60c4cd699eaa
Bradley Golden <bradleygolden@Bradleys-MacBook-Pro.local>
2015-10-11 14:50:43 -0500

    added method specifications

---
+++ backend/objects/TestDriver.java
@@ -0,0 +1,26 @@
+// Programmers:  Stephen Selke, Bradley Golden, Chris Griffith
+// Assignment:  Project 2, Monopoly
+// Date:        October 6, 2015
+// Description:
+//
+
+public class TestDriver
+{
+    public static void main()
+    {
+        BoardLocation[] board = new BoardLocation[40];    
+
+        // All board locations are initialized in the order in which they are displayed on the Monopoly Board
+        // starting from go.
+        
+        // CornerSquare(name, address)
+        // Lot(name, address, color, price, base rent)
+        // CardSquare(name, address)
+        // TaxSquare(name, address) 
+        // Railroad(name, address)
+        // Utility(name, address)
+        board[0] = new BoardLocation("Go", 0);
+        board[1] = new Lot("")
+    }
+}
+
