7d3d795e130f75dd65397139ec283aa536e0b4d5
Bradley Golden <bradleygolden@Bradleys-MacBook-Pro.local>
2015-10-11 15:55:58 -0500

    added toString method, removed methods that relate to player

--- backend/objects/Utility.java
+++ backend/objects/Utility.java
@@ -17,53 +17,22 @@ public class Utility extends Property
         super();
         super.price = 150; // price of a utility
         super.baseRent = 1; // the baseRent without the dice multplier
-
-        ownerProperties = super.owner.getProperties(); // Get an array of properties this owner owns
-        numUtilitiesOwned = this.getNumUtilitiesOwned(); // Get number of utilites owned by this owner
     }
 
-    // PRE: Name is the name of the utility at the given board location
-    // POST: Utility object is created with name set to the name
-    public Utility(String name)
+    // PRE: Name is the name and address of the utility at the given board location
+    //      address = 12 or 28
+    // POST: Utility object is created with name = name and address = address
+    public Utility(String name, int address)
     {
         super();
         super.name = name; // set the name of this utilty
-    }
-
-    // POST: FCTVAL == Number of utlities owned by the current owner
-    //                 Integer value in dollars where count >= 0 and count <= 2
-    private int getNumUtilitiesOwned()
-    {
-        int count = 0;
-
-        for (Property aProperty : ownerProperties) // loop through all properties owned by owner
-        {
-            if (aProperty instanceof Utility) // check if a property is a utility
-            {
-                count++; // increment count if owner owns another utility
-            }
-        }
-
-        return count; // return count of utilities
+        super.address = address; // set the address of this utilty
     }
 
     // POST: FCTVAL == The current rent of landing on this utility
     public int getRent()
     {
-        int diceRoll = 0; //TODO - Need to obtain the dice roll value from the player roll
-
-        // check if number of utilities owned by the player is 1
-        if (this.numUtilitiesOwned == 1)
-        {
-            return baseRent * (diceRoll*4); // rent is dice roll value * 4
-        }
-        // check if the number of utilities owned by the player is 2
-        else if (this.numUtilitiesOwned == 2)
-        {
-            return baseRent * (diceRoll*10); // rent is dice roll value * 10
-        }
-
-        return 0; // utility isn't owned by a player, rent is 0
+        return baseRent;
     }
 
     public int sell()
@@ -93,5 +62,15 @@ public class Utility extends Property
     {
         super.owner = buyer;
         super.owner.withdraw(super.baseRent);
+    }
+
+    // POST: A string containing the name, rent, and address of the utility at this boardlocation
+    @Override 
+    public String toString()
+    {
+        return "Name: " + this.name + "\t" +
+            "Address: " + this.getRent() + "\t" +
+            "Rent:" + this.getRent() + "\t" +
+            "";
     }
 }
