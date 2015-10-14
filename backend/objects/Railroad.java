acc626f93ce8246ebe6d3657a75987b7fe1c4e81
Bradley Golden <bradleygolden@10-4-228-3.vlan890.client.wireless.uic.edu>
2015-10-14 10:33:39 -0500

    added test

--- backend/objects/Railroad.java
+++ backend/objects/Railroad.java
@@ -52,4 +52,34 @@ public class Railroad extends Property
             return -1; // an error has occurred
         }
     }
+    public static void main(String[] args)
+    {
+        // create railroad space
+        BoardLocation[] board = new BoardLocation[1];
+
+        // initalize with a railroad
+        board[0] = new Railroad("Reading Railroad", 5);
+
+        // print railroad details
+        System.out.println(board[0].toString());
+
+        // create mock player
+        Player player = new Player(300, "Boot");
+
+        // create mock owner
+        Player owner = new Player(500, "Thimble");
+
+        // have owner purchase that property
+        //if(board[0].buy(owner))
+        //{
+            //// do nothing
+        //} 
+
+        //else 
+        //{
+            ////System.out.println("Error adding owner");
+        //}
+
+        //// simulate player landing on railroad
+    }
 }
